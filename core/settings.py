e8c26e291ccad9ac683b54a96098cb8ceb22c4b8
MIMICHKA <michkail.m@wingscorp.com>
2024-10-09 12:10:27 +0700

    ....

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SEC')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(",")
 
